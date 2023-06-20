ed1b00cee1a206da5ec5c1533a3f4ae748efe5e6
Neil MacLean <nbmac13@gmail.com>
2023-06-20 10:09:02 -0500

    Removed unused ApplicationView routes

--- the-enchiridion/src/components/views/ApplicationViews.js
+++ the-enchiridion/src/components/views/ApplicationViews.js
@@ -23,9 +23,6 @@ export const ApplicationViews = () => {
           <Route path="/playlists" element={<Playlists />} />
           <Route path="/playlists/create" element={<PlaylistForm />} />
           <Route path="/playlists/:playlistId/edit" element={<PlaylistForm />} />
-          <Route path="/seasons/:seasonNumber/episodes/:episodeNumber" element={<EpisodeDetail />} />
-          <Route path="/seasons/:seasonNumber" element={<SeasonDetail />} />
-          <Route path="/seasons" element={<Seasons />} />
           <Route path="/search" element={<SearchBar />} />
           <Route path="/search/:resultId" element={<SearchDetail />} />
           <Route path="/search/:resultId/season/:seasonNumber" element={<SeasonDetail />} />
