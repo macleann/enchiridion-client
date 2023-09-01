6d1b687defd523af377c833a7cb90e9e33b9d6ff
Neil MacLean <nbmac13@gmail.com>
2023-09-01 12:05:02 -0500

    Installed material ui to help with styling

--- the-enchiridion/src/components/nav/NavBar.js
+++ the-enchiridion/src/components/nav/NavBar.js
@@ -1,5 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
 
 export const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +16,6 @@ export const NavBar = () => {
     const height = navBar.offsetHeight;
     document.documentElement.style.setProperty('--navbar-height', `${height}px`);
   }, [isOpen]);
-  
 
   const handleLogout = () => {
     localStorage.removeItem("enchiridion_user");
@@ -19,7 +24,38 @@ export const NavBar = () => {
     setIsOpen(false);
   };
 
-  const handleClick = () => setIsOpen(false);
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setIsOpen(open);
+  };
+
+  const list = () => (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => navigate("/playlists")}>
+            <ListItemText primary="Playlists" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => navigate("/search")}>
+            <ListItemText primary="Search Shows" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleLogout}>
+            <ListItemText primary="Logout" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  );
 
   return (
     <div id="navBar" className="md:flex md:items-center p-5 bg-white bg-opacity-50">
@@ -27,17 +63,24 @@ export const NavBar = () => {
         <Link to="/">
           <img className="w-16 h-auto" src="/Enchiridion.png" alt="Enchiridion" />
         </Link>
-        <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        <button onClick={toggleDrawer(true)} className="md:hidden">
           {/* Hamburger Icon */}
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
+        <Drawer
+          anchor="right"
+          open={isOpen}
+          onClose={toggleDrawer(false)}
+        >
+          {list()}
+        </Drawer>
       </div>
-      <div className={`${isOpen ? 'block' : 'hidden'} md:flex md:w-full md:justify-between`}>
+      <div className="hidden md:flex md:w-full md:justify-between">
         <div className="md:justify-start md:ml-4">
-          <Link to="/playlists" onClick={handleClick} className="block mt-4 ml-2 md:inline-block md:mt-0 mr-6 font-bold">Playlists</Link>
-          <Link to="/search" onClick={handleClick} className="block mt-4 ml-2 md:inline-block md:mt-0 mr-6 font-bold">Search Shows</Link>
+          <Link to="/playlists" className="block mt-4 ml-2 md:inline-block md:mt-0 mr-6 font-bold">Playlists</Link>
+          <Link to="/search" className="block mt-4 ml-2 md:inline-block md:mt-0 mr-6 font-bold">Search Shows</Link>
         </div>
         <div className="md:justify-end md:mr-4">
           {localStorage.getItem("enchiridion_user") ? (
